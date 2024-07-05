b0c3423e76da46ce0ea63bbd69f6ad37f4f514b2
Daril Rodriguez <darilrodriguez.2@gmail.com>
2024-07-05 12:39:09 -0300

    UPDATE DLL.H

--- include/dll.hpp
+++ include/dll.hpp
@@ -1,17 +1,23 @@
 #pragma once
 
-#if defined(_MSC_VER)
+#if defined(__clang__)
+
+#define AMUSE_API
+
+#elif defined(__GNUC__) || defined(__GNUG__)
+
+#ifdef AMUSE_CORE_EXPORT
+#define AMUSE_API __attribute__((visibility("default")))
+#else
+#define AMUSE_API
+#endif // AMUSE_CORE_EXPOR
+
+#elif defined(_MSC_VER)
+
 #ifdef AMUSE_CORE_EXPORT
 #define AMUSE_API __declspec(dllexport)
 #else
 #define AMUSE_API __declspec(dllimport)
 #endif // AMUSE_CORE_EXPORT
-#elif defined(__GNUC__)
-#ifdef AMUSE_CORE_EXPORT
-#define AMUSE_API __attribute__((visibility("default")))
-#else
-#define AMUSE_API
-#endif // AMUSE_CORE_EXPORT
-#else
-#define AMUSE_API
+
 #endif
